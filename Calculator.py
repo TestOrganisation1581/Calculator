025593387adadf05dfa73e4667be1e998ae9531d
Kaktus200020 <artemkremnev2007@gmail.com>
2024-03-01 23:12:38 +0300

    readyCommit

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,23 @@
-print("!!!?")
+class Calculator:
+    el1=0
+    el2=0
+    operation=""
+    ans=0
+    def SetParams(self):
+        print("Введите первый элемент")
+        self.el1=int(input())
+        print("Введите операцию(+; -;)")
+        self.operation=input()
+        print("Введите второй элемент")
+        self.el2=int(input())
+    def Work(self):
+        if(self.operation=="+"):
+            ans=self.el1+self.el2
+        else:
+            ans=self.el1-self.el2
+        return ans
+C=Calculator()
+C.SetParams()
+print(C.Work())
+
+        
